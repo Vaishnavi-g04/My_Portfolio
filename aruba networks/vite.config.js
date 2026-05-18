ff45f4ac22945f34404fe8699720fd882cf1a212
Vaishnavi G <vaishugckm@gmail.com>
2026-05-18 22:11:57 +0530

    Add login app build

--- aruba networks/vite.config.js
+++ aruba networks/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/My_Portfolio/login-app/',
+  build: {
+    outDir: '../login-app',
+    emptyOutDir: true,
+  },
   server: {
     port: 5174,
     strictPort: true,
